ffc579e9108758e9d3eb71078108ec1c1baf91e5
wlsfernandes <wlsfernandes@gmail.com>
2026-01-14 17:20:55 -0500

    fix csss

--- resources/views/frontend/partials/who-we-are.blade.php
+++ resources/views/frontend/partials/who-we-are.blade.php
@@ -1,7 +1,7 @@
 @if (!empty($aboutSections['header']))
   <section class="section__bg legacy-section">
     <div class="title__content center wow fadeInUp" data-wow-duration="1.3s">
-      <h1 class="heading-gradient-green-black">{{ $aboutSections['header']->{'title_' . app()->getLocale()} ?? '' }}</h1>
+      <h2 class="heading-gradient-green-black">{{ $aboutSections['header']->{'title_' . app()->getLocale()} ?? '' }}</h2>
       <h5>{{ $aboutSections['header']->{'subtitle_' . app()->getLocale()} ?? '' }}</h5>
       <div class="legacy-content">
         {!! $aboutSections['header']->{'content_' . app()->getLocale()} ?? '' !!}
@@ -13,15 +13,7 @@
   <section class="team__details overhid section-spacing">
     <div class="container">
       <div class="row g-5">
-        <div class="col-xxl-5 col-xl-5 col-lg-8">
-          <div class="team__left">
-            <div class="details__thumb">
-              <!-- <img class="img-fluid w-75 mx-auto d-block" -->
-              <img
-                src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['new_way']->id]) }}"alt="about__image">
-            </div>
-          </div>
-        </div>
+
         <div class="col-xxl-7 col-xl-7 col-lg-10">
           <div class="about__header heading-gradient-green-black">
             <h2 class="heading-gradient-green-black">
@@ -36,7 +28,15 @@
             </p>
           </div>
         </div>
-
+        <div class="col-xxl-5 col-xl-5 col-lg-8">
+          <div class="team__left">
+            <div class="details__thumb">
+              <!-- <img class="img-fluid w-75 mx-auto d-block" -->
+              <img
+                src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['new_way']->id]) }}"alt="about__image">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
