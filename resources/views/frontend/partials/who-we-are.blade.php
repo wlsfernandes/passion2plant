df1b1d9473407ef63ef4a72ccac64a3f83167e83
wlsfernandes <wlsfernandes@gmail.com>
2026-01-14 16:51:24 -0500

    new way

--- resources/views/frontend/partials/who-we-are.blade.php
+++ resources/views/frontend/partials/who-we-are.blade.php
@@ -9,7 +9,38 @@
     </div>
   </section>
 @endif
+@if (!empty($aboutSections['new_way']))
+  <section class="team__details overhid section-spacing">
+    <div class="container">
+      <div class="row g-5">
+        <div class="col-xxl-5 col-xl-5 col-lg-8">
+          <div class="team__left">
+            <div class="details__thumb">
+              <!-- <img class="img-fluid w-75 mx-auto d-block" -->
+              <img
+                src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['problem']->id]) }}"alt="about__image">
+            </div>
+          </div>
+        </div>
+        <div class="col-xxl-7 col-xl-7 col-lg-10">
+          <div class="about__header heading-gradient-green-black">
+            <h2 class="heading-gradient-green-black">
+              {{ $aboutSections['new_way']->{'title_' . app()->getLocale()} ?? '' }}</h2>
+          </div>
+          <div class="details__cont" style="margin-top: 20px;">
+            <h5>
+              {{ $aboutSections['new_way']->{'subtitle_' . app()->getLocale()} ?? '' }}
+            </h5>
+            <p style="margin-top: 20px;">
+              {!! $aboutSections['new_way']->{'content_' . app()->getLocale()} ?? '' !!}
+            </p>
+          </div>
+        </div>
 
+      </div>
+    </div>
+  </section>
+@endif
 @if (!empty($aboutSections['who_we_are']))
   <section class="team__details overhid section-spacing">
     <div class="container">
@@ -90,7 +121,8 @@
           <div class="team__left">
             <div class="details__thumb">
               <!-- <img class="img-fluid w-75 mx-auto d-block" -->
-              <img src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['values']->id]) }}"
+              <img
+                src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['values']->id]) }}"
                 alt="about__image">
             </div>
           </div>
