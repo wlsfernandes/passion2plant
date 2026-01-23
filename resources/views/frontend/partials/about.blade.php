bbaae6f9352dc01b15a2ff7c3369761d753d85d5
wlsfernandes <wlsfernandes@gmail.com>
2026-01-23 11:58:34 -0500

    bg images button sections

--- resources/views/frontend/partials/about.blade.php
+++ resources/views/frontend/partials/about.blade.php
@@ -1,37 +1,37 @@
 <!--About Section Here-->
 <section class="about__section pt-130 pb-130 overhid">
-  <div class="container">
-    <div class="row g-4 align-items-center justify-content-between">
-      <div class="col-xxl-6 col-xl-6 col-lg-6 wow fadeInLeft" data-wow-duration="3s">
-        <div class="about__thumb">
-          <img src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['vision']->id]) }}"
-            alt="about__image">
-          <div class="small__about__one">
-            <img src="{{ asset('assets/frontend/img/about/about-small1.jpg') }}" alt="about__img">
-          </div>
-          <div class="small__about">
-            <img src="{{ asset('assets/frontend/img/about/about-small.jpg') }}" alt="about__img">
-          </div>
-        </div>
-      </div>
-      <div class="col-xxl-6 col-xl-6 col-lg-6 wow fadeInRight" data-wow-duration="5s">
-        <div class="about__content">
-          <div class="about__header">
-            <h6>
-              @lang('home.about_us')
-            </h6>
-          </div>
-          <h2 class="mb-4">
-            {{ $aboutSections['vision']->{'title_' . app()->getLocale()} ?? '' }}
+    <div class="container">
+        <div class="row g-4 align-items-center justify-content-between">
+            <div class="col-xxl-6 col-xl-6 col-lg-6 wow fadeInLeft" data-wow-duration="3s">
+                <div class="about__thumb">
+                    <img src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['vision']->id]) }}"
+                        alt="about__image">
+                    <div class="small__about__one">
+                        <img src="{{ asset('assets/frontend/img/about/b4.jpg') }}" alt="about__img">
+                    </div>
+                    <div class="small__about">
+                        <img src="{{ asset('assets/frontend/img/about/b3.jpg') }}" alt="about__img">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-6 col-xl-6 col-lg-6 wow fadeInRight" data-wow-duration="5s">
+                <div class="about__content">
+                    <div class="about__header">
+                        <h6>
+                            @lang('home.about_us')
+                        </h6>
+                    </div>
+                    <h2 class="mb-4">
+                        {{ $aboutSections['vision']->{'title_' . app()->getLocale()} ?? '' }}
 
-          </h2>
-          <h4>
-            {!! $aboutSections['vision']->{'subtitle_' . app()->getLocale()} ?? '' !!}
-          </h4>
-          <p style="margin-top: 20px;">
-            {!! $aboutSections['vision']->{'content_' . app()->getLocale()} ?? '' !!}
-          </p>
-          <!--  <div class="profile__item d-flex align-items-center mt-5">
+                    </h2>
+                    <h4>
+                        {!! $aboutSections['vision']->{'subtitle_' . app()->getLocale()} ?? '' !!}
+                    </h4>
+                    <p style="margin-top: 20px;">
+                        {!! $aboutSections['vision']->{'content_' . app()->getLocale()} ?? '' !!}
+                    </p>
+                    <!--  <div class="profile__item d-flex align-items-center mt-5">
                         <div class="profile d-flex align-items-center">
                             <div class="profile__thumb">
                                 <img src="{{ asset('assets/frontend/img/about/profile/Dra.ElizabethRios.webp') }}" alt="profile__image">
@@ -43,10 +43,10 @@
                         </div>
                         <a href="#" class="cmn--btn">@lang('home.read_more')</a>
                     </div> -->
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
 <!--Team Details Section Here
 <section class="team__details overhid">
