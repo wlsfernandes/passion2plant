8b5b92c38e7f9210fa27808293be56b28acb61fd
wlsfernandes <wlsfernandes@gmail.com>
2026-02-16 15:22:28 -0500

    fix media view

--- resources/views/frontend/media-types/index.blade.php
+++ resources/views/frontend/media-types/index.blade.php
@@ -4,67 +4,76 @@
 
 @section('content')
 
-    @php
-        use Illuminate\Support\Str;
-    @endphp
+@php use Illuminate\Support\Str; @endphp
 
-    <section class="service__section section__bg pt-130 pb-130 overhid">
-        <div class="container">
-            <div class="row g-4">
+<section class="details__section pt-130 pb-130 overhid">
+    <div class="container">
+        <div class="row justify-content-center g-4">
 
-                @forelse ($mediaTypes as $type)
-                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 wow fadeInUp"
-                        data-wow-duration="{{ 3 + ($loop->index % 3) * 2 }}s">
+            @forelse ($mediaTypes as $type)
 
-                        <div class="service__items center">
+                <div class="col-12 col-sm-6 col-lg-4 d-flex">
 
-                            {{-- Image --}}
-                            <div class="thumb">
-                                <a href="{{ route('media.byType', $type->slug) }}">
-                                    @if ($type->image_url)
-                                        <img src="{{ route('admin.images.preview', [
+                    <div class="details__items text-center w-100 d-flex flex-column">
+
+                        {{-- Image --}}
+                        <div class="details__thumb mb-3">
+                            <a href="{{ route('media.byType', $type->slug) }}">
+
+                                @if ($type->image_url)
+                                    <img
+                                        src="{{ route('admin.images.preview', [
                                             'model' => 'media-types',
                                             'id' => $type->id,
                                         ]) }}"
-                                            alt="{{ $type->name }}" loading="lazy">
-                                    @else
-                                        <div class="d-flex align-items-center justify-content-center"
-                                            style="height:220px;background:#f5f5f5;">
-                                            <i class="uil uil-play-circle text-muted font-size-40"></i>
-                                        </div>
-                                    @endif
-                                </a>
-                            </div>
-
-                            {{-- Content --}}
-                            <div class="content">
-                                <h5>
-                                    <a href="{{ route('media.byType', $type->slug) }}">
-                                        {{ $type->name }}
-                                    </a>
-                                </h5>
-
-                                @if ($type->description)
-                                    <p>
-                                        {{ Str::limit(strip_tags($type->description), 120) }}
-                                    </p>
+                                        alt="{{ $type->name }}"
+                                        class="img-fluid w-100"
+                                        loading="lazy">
+                                @else
+                                    <div class="d-flex align-items-center justify-content-center media-placeholder">
+                                        <i class="uil uil-play-circle text-muted"></i>
+                                    </div>
                                 @endif
 
-                                <a href="{{ route('media.byType', $type->slug) }}" class="btns">
-                                    @lang('pages.explore')
+                            </a>
+                        </div>
+
+                        {{-- Content --}}
+                        <div class="details__content px-3 d-flex flex-column flex-grow-1">
+
+                            <h5 class="mb-2">
+                                <a href="{{ route('media.byType', $type->slug) }}">
+                                    {{ $type->name }}
                                 </a>
-                            </div>
+                            </h5>
+
+                            @if ($type->description)
+                                <p class="flex-grow-1">
+                                    {{ Str::limit(strip_tags($type->description), 120) }}
+                                </p>
+                            @else
+                                <p class="flex-grow-1"></p>
+                            @endif
+
+                            <a href="{{ route('media.byType', $type->slug) }}"
+                               class="cmn--btn mt-3">
+                                @lang('pages.explore')
+                            </a>
 
                         </div>
-                    </div>
-                @empty
-                    <div class="col-12 text-center text-muted">
-                        @lang('pages.no_media_available')
-                    </div>
-                @endforelse
 
-            </div>
+                    </div>
+
+                </div>
+
+            @empty
+                <div class="col-12 text-center text-muted">
+                    @lang('pages.no_media_available')
+                </div>
+            @endforelse
+
         </div>
-    </section>
+    </div>
+</section>
 
 @endsection
