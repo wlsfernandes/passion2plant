f17b551da37029ed9e231b9bf4bb659604bf29c0
wlsfernandes <wlsfernandes@gmail.com>
2026-03-11 14:05:12 -0400

    fix vision about section

--- resources/views/frontend/partials/who-we-are.blade.php
+++ resources/views/frontend/partials/who-we-are.blade.php
@@ -218,3 +218,33 @@
         </div>
     </section>
 @endif
+@if (!empty($aboutSections['vision']))
+    <section class="team__details overhid section-spacing">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-xxl-7 col-xl-7 col-lg-10">
+                    <div class="about__header heading-gradient-green-black">
+                        <h2 class="heading-gradient-green-black" data-wow-duration="1.3s">
+                            {{ $aboutSections['vision']->{'title_' . app()->getLocale()} ?? '' }}</h2>
+                    </div>
+                    <div class="details__cont" style="margin-top: 20px;">
+                        <p style="margin-top: 20px;">
+                        <div class="cms-content" id="cms-content">
+                            {!! $aboutSections['vision']->{'content_' . app()->getLocale()} ?? '' !!}
+                        </div>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xxl-5 col-xl-5 col-lg-8">
+                    <div class="team__left">
+                        <div class="details__thumb">
+                            <!-- <img class="img-fluid w-75 mx-auto d-block" -->
+                            <img src="{{ route('admin.images.preview', ['model' => 'about', 'id' => $aboutSections['vision']->id]) }}"
+                                alt="about__image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endif
