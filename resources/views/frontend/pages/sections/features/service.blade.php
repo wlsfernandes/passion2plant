14bd04572ef92a66be09ff4242046601ac58e9c8
wlsfernandes <wlsfernandes@gmail.com>
2026-03-27 07:03:10 -0400

    service blade

--- resources/views/frontend/pages/sections/features/service.blade.php
+++ resources/views/frontend/pages/sections/features/service.blade.php
@@ -1,43 +1,57 @@
-    @php use Illuminate\Support\Str; @endphp
-    <section class="details__section pt-130 pb-130 overhid">
-        <div class="container">
-            @include('frontend.pages.sections.partials.content')
-            <div class="row justify-content-center g-4">
-                @forelse($services as $service)
-                    <div class="col-12 col-sm-6 col-lg-4 d-flex">
-                        <div class="details__items text-center w-100 d-flex flex-column">
-                            <div class="details__thumb mb-3">
+@php
+    use Illuminate\Support\Str;
+    $isSingle = $services->count() === 1;
+@endphp
+
+<section class="blog__section pt-130 pb-130 overhid">
+    <div class="container">
+        @include('frontend.pages.sections.partials.content')
+
+        <div class="row g-4">
+
+            @forelse($services as $service)
+                <div
+                    class="{{ $isSingle ? 'col-md-6 col-lg-4 mx-auto' : 'col-xxl-4 col-xl-4 col-lg-4 col-md-6' }} wow fadeInUp">
+
+                    <div class="blog__items">
+
+                        {{-- Image --}}
+                        <div class="thumb">
+                            <a href="{{ route('services.display', $service->slug) }}">
+                                <img src="{{ route('admin.images.preview', ['model' => 'services', 'id' => $service->id]) }}"
+                                    alt="{{ $service->getTitle() }}" loading="lazy">
+                            </a>
+                        </div>
+
+                        {{-- Content --}}
+                        <div class="content">
+
+                            <h5>
                                 <a href="{{ route('services.display', $service->slug) }}">
-                                    <img src="{{ route('admin.images.preview', ['model' => 'services', 'id' => $service->id]) }}"
-                                        alt="{{ $service->getTitle() }}" class="img-fluid w-100" loading="lazy">
+                                    {{ $service->getTitle() }}
                                 </a>
-                            </div>
-                            <div class="details__content px-3 d-flex flex-column flex-grow-1">
+                            </h5>
 
-                                <h5 class="mb-2">
-                                    <a href="{{ route('services.display', $service->slug) }}">
-                                        {{ $service->getTitle() }}
-                                    </a>
-                                </h5>
+                            <p>
+                                {{ Str::limit(strip_tags($service->getContent()), 120) }}
+                            </p>
 
-                                <p class="flex-grow-1">
-                                    {{ Str::limit(strip_tags($service->getContent()), 120) }}
-                                </p>
-
-                                <a href="{{ route('services.display', $service->slug) }}" class="cmn--btn mt-3">
-                                    @lang('pages.read_more')
-                                </a>
-
-                            </div>
+                            <a href="{{ route('services.display', $service->slug) }}"
+                                class="btn btn-sm btn-outline-success mt-2">
+                                @lang('pages.read_more')
+                            </a>
 
                         </div>
 
                     </div>
-                @empty
-                    <div class="col-12 text-center text-muted">
-                        @lang('pages.no_services_available')
-                    </div>
-                @endforelse
-            </div>
+                </div>
+
+            @empty
+                <div class="col-12 text-center text-muted">
+                    @lang('pages.no_services_available')
+                </div>
+            @endforelse
+
         </div>
-    </section>
+    </div>
+</section>
