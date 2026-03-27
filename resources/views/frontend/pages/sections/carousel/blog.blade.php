6771d2dc20abf0a34b62db8b0f7cb7facdbd9aff
wlsfernandes <wlsfernandes@gmail.com>
2026-03-27 05:07:53 -0400

    fix blog center

--- resources/views/frontend/pages/sections/carousel/blog.blade.php
+++ resources/views/frontend/pages/sections/carousel/blog.blade.php
@@ -1,9 +1,17 @@
+@php
+    $isSingle = $featuredBlogs->count() === 1;
+@endphp
+
 <section class="blog__section pt-130 pb-130 overhid">
     <div class="container">
         @include('frontend.pages.sections.partials.content')
+
         <div class="row g-4">
+
             @foreach ($featuredBlogs as $blog)
-                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 wow fadeInUp">
+                <div
+                    class="{{ $isSingle ? 'col-md-6 col-lg-4 mx-auto' : 'col-xxl-4 col-xl-4 col-lg-4 col-md-6' }} wow fadeInUp">
+
                     <div class="blog__items">
 
                         <div class="thumb">
@@ -12,24 +20,28 @@
                                     alt="">
                             </a>
                         </div>
+
                         <div class="content">
                             <h5>
                                 <a href="{{ route('blogs.display', $blog->slug) }}">
                                     {{ $blog->getTitle() }}
                                 </a>
                             </h5>
+
                             <p>
                                 {{ $blog->limitText($blog->getContent(), 120) }}
                             </p>
+
                             <a href="{{ route('blogs.display', $blog->slug) }}"
                                 class="btn btn-sm btn-outline-success mt-2">
                                 @lang('pages.read_more')
                             </a>
                         </div>
+
                     </div>
                 </div>
             @endforeach
-        </div>
 
+        </div>
     </div>
 </section>
