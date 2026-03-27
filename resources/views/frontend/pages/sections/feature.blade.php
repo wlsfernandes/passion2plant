116483514365053dc423bc20a384aa2d5288af2c
wlsfernandes <wlsfernandes@gmail.com>
2026-03-27 06:46:16 -0400

    feature contact

--- resources/views/frontend/pages/sections/feature.blade.php
+++ resources/views/frontend/pages/sections/feature.blade.php
@@ -5,6 +5,7 @@
         'position' => 'frontend.pages.sections.features.position',
         'services' => 'frontend.pages.sections.features.service',
         'team' => 'frontend.pages.sections.features.team',
+        'contact' => 'frontend.pages.sections.features.contact',
         default => null,
     };
 @endphp
