2ea3ccd2db5f59429a5a795377066dc01ae8164b
wlsfernandes <wlsfernandes@gmail.com>
2026-03-27 05:01:07 -0400

    donation block

--- resources/views/frontend/pages/sections/carousel/donate.blade.php
+++ resources/views/frontend/pages/sections/carousel/donate.blade.php
@@ -3,29 +3,31 @@
     $isSingle = $donations->count() === 1;
 @endphp
 
-<section class="service__section section__bg pt-130 pb-130 overhid">
+<section class="blog__section pt-130 pb-130 overhid">
     <div class="container">
         @include('frontend.pages.sections.partials.content')
+
         <div class="row g-4">
+
             @forelse($donations as $donation)
-                <div class="{{ $isSingle ? 'col-12 d-flex justify-content-center' : 'col-xxl-4 col-xl-4 col-lg-4 col-md-6' }} wow fadeInUp"
+                <div class="{{ $isSingle ? 'col-md-6 col-lg-4 mx-auto' : 'col-xxl-4 col-xl-4 col-lg-4 col-md-6' }} wow fadeInUp"
                     data-wow-duration="{{ 3 + ($loop->index % 3) * 2 }}s">
 
-                    <div class="service__items center {{ $isSingle ? 'mx-auto' : '' }}"
-                        style="{{ $isSingle ? 'max-width:400px;' : '' }}">
+                    <div class="blog__items">
 
                         {{-- Image --}}
                         <div class="thumb">
-                            <img src="{{ route('admin.images.preview', ['model' => 'donations', 'id' => $donation->id]) }}"
-                                alt="{{ $donation->title }}" loading="lazy">
+                            <a href="{{ route('donations.checkout', $donation) }}">
+                                <img src="{{ route('admin.images.preview', ['model' => 'donations', 'id' => $donation->id]) }}"
+                                    alt="{{ $donation->title }}" loading="lazy">
+                            </a>
                         </div>
 
                         {{-- Content --}}
                         <div class="content">
 
-                            <h5 class="mb-1">
-                                <a href="{{ route('donations.checkout', $donation) }}"
-                                    class="text-decoration-none text-dark">
+                            <h5>
+                                <a href="{{ route('donations.checkout', $donation) }}">
                                     {{ $donation->title }}
                                 </a>
                             </h5>
@@ -36,20 +38,19 @@
                                 </span>
                             @endif
 
-                            <p class="mt-2">
+                            <p>
                                 {{ Str::limit(strip_tags($donation->description), 120) }}
                             </p>
 
-                            <a href="{{ route('donations.checkout', $donation) }}"
-                                class="btn btn-sm btn-outline-success mt-2">
+                            <a href="{{ route('donations.checkout', $donation) }}" class="btns">
                                 Give Now
                             </a>
 
                         </div>
 
-
                     </div>
                 </div>
+
             @empty
                 <div class="col-12 text-center text-muted">
                     @lang('pages.no_services_available')
