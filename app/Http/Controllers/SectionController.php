1a9c4ec64f171491cce3c25e3758044b813472f2
wlsfernandes <wlsfernandes@gmail.com>
2026-04-03 09:50:14 -0400

    fix services error

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -21,8 +21,8 @@ class SectionController extends BaseController
     {
         return $request->validate([
             'sort_order' => ['nullable', 'integer'],
-            'feature_type' => 'nullable|string|in:team,partners,testimonial,service,blog,donate,event,contact,position,resource,book',
-            'carousel_type' => 'nullable|string|in:team,partners,testimonial,service,blog,donate,event,contact,position,resource,book',
+            'feature_type' => 'nullable|string|in:team,partners,testimonial,services,blog,donate,event,contact,position,resource,book',
+            'carousel_type' => 'nullable|string|in:team,partners,testimonial,services,blog,donate,event,contact,position,resource,book',
             'title_en' => ['nullable', 'string', 'max:255'],
             'title_es' => ['nullable', 'string', 'max:255'],
             'content_en' => ['nullable', 'string'],
