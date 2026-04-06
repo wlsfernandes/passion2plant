286a197b4bdb062e396f402e2ae909cb54eced1d
wlsfernandes <wlsfernandes@gmail.com>
2026-04-06 13:13:58 -0400

    tem balde

--- resources/views/frontend/pages/sections/carousel/team.blade.php
+++ resources/views/frontend/pages/sections/carousel/team.blade.php
@@ -2,18 +2,22 @@
 <section class="team__section pt-130 pb-130 overhid" style="{{ $section->style }}">
     <div class="container">
         @include('frontend.pages.sections.partials.content')
-        <div class="row g-4">
+        <div class="row g-4 justify-content-center">
             @foreach ($featuredTeams as $team)
                 <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-4 wow fadeInUp"
                     data-wow-duration="{{ 3 + $loop->index * 2 }}s">
+
                     <div class="team__items position-relative">
                         <a href="{{ route('team.profile', strip_tags($team->slug)) }}" class="stretched-link"></a>
+
                         <div class="team__thumb">
                             <img src="{{ route('admin.images.preview', ['model' => 'teams', 'id' => $team->id]) }}"
                                 alt="{!! $team->name !!}" loading="lazy">
                         </div>
+
                         <div class="team__content">
                             <div class="cms-html">{!! $team->name !!}</div>
+
                             <div class="team__meta d-flex justify-content-center flex-wrap gap-5 mt-3">
                                 @foreach ($team->sectors as $sector)
                                     <span class="badge bg-light text-dark px-3 py-2 mt-2">
@@ -23,6 +27,7 @@
                             </div>
                         </div>
                     </div>
+
                 </div>
             @endforeach
         </div>
