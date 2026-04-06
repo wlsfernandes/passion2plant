e0cc1a651b448299f254ba35dc57b8b3b4c464ff
wlsfernandes <wlsfernandes@gmail.com>
2026-04-06 13:09:51 -0400

    team carousel

--- resources/views/frontend/pages/sections/carousel/team.blade.php
+++ resources/views/frontend/pages/sections/carousel/team.blade.php
@@ -1,45 +1,48 @@
 <!--Team Section Here-->
-<section class="team__section pt-130 pb-130 overhid" style="{{ $section->style }}">
+<section class="team__section overhid pt-130 pb-130" style="{{ $section->style }}">
     <div class="container">
+
         @include('frontend.pages.sections.partials.content')
 
-        <div class="swiper team__carousel">
+        <div class="swiper testimonial__wrapper">
             <div class="swiper-wrapper">
 
                 @foreach ($featuredTeams as $team)
                     <div class="swiper-slide">
-                        <div class="team__items position-relative">
-                            <a href="{{ route('team.profile', strip_tags($team->slug)) }}" class="stretched-link"></a>
+                        <div class="testi__items">
 
-                            <div class="team__thumb">
-                                <img src="{{ route('admin.images.preview', ['model' => 'teams', 'id' => $team->id]) }}"
-                                    alt="{!! $team->name !!}" loading="lazy">
-                            </div>
+                            <div class="testi__wrap">
+                                <div class="testi__thumb">
+                                    <img src="{{ route('admin.images.preview', ['model' => 'teams', 'id' => $team->id]) }}"
+                                        alt="{!! strip_tags($team->name) !!}" loading="lazy"
+                                        onerror="this.src='/images/default-avatar.png';">
+                                </div>
 
-                            <div class="team__content">
-                                <div class="cms-html">{!! $team->name !!}</div>
+                                <div class="content">
+                                    <h6 class="cms-html">
+                                        {!! $team->name !!}
+                                    </h6>
 
-                                <div class="team__meta d-flex justify-content-center flex-wrap gap-5 mt-3">
-                                    @foreach ($team->sectors as $sector)
-                                        <span class="badge bg-light text-dark px-3 py-2 mt-2">
-                                            {!! $sector->name !!}
-                                        </span>
-                                    @endforeach
+                                    <span>
+                                        @foreach ($team->sectors as $sector)
+                                            {!! $sector->name !!}{{ !$loop->last ? ',' : '' }}
+                                        @endforeach
+                                    </span>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 @endforeach
 
             </div>
 
-            <!-- Optional navigation -->
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
-
-            <!-- Optional pagination -->
-            <div class="swiper-pagination"></div>
+            <!-- dots (already styled in your theme) -->
+            <div class="swiper-dot text-center pt-5">
+                <div class="dot"></div>
+            </div>
         </div>
+
     </div>
 </section>
 <!--Team Section End-->
