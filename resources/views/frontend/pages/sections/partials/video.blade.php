5fb63bc04a46e5eced493462f35920de103975ea
wlsfernandes <wlsfernandes@gmail.com>
2026-04-09 19:41:00 -0400

    video player

--- resources/views/frontend/pages/sections/partials/video.blade.php
+++ resources/views/frontend/pages/sections/partials/video.blade.php
@@ -1,19 +1,64 @@
-    @if ($section->image_url)
-        <div class="section-video text-center position-relative">
+@if ($section->image_url)
+    <div class="section-video text-center position-relative">
 
-            <a href="{{ $section->link_image }}" target="_blank" class="video-link" target="blank">
+        <a href="#" data-bs-toggle="modal" data-bs-target="#videoModal">
+            <img src="{{ route('admin.images.preview', [
+                'model' => 'sections',
+                'id' => $section->id,
+            ]) }}"
+                class="img-fluid rounded shadow-sm w-100">
 
-                <img src="{{ route('admin.images.preview', [
-                    'model' => 'sections',
-                    'id' => $section->id,
-                ]) }}"
-                    alt="{{ $section->title ?? 'Section image' }}" class="img-fluid rounded shadow-sm w-100">
+            <span class="video-play-icon">
+                <i class="fas fa-play"></i>
+            </span>
+        </a>
 
-                <span class="video-play-icon">
-                    <i class="fas fa-play"></i>
-                </span>
+    </div>
 
-            </a>
+    <!-- Modal -->
+    <div class="modal fade" id="videoModal" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
 
+                <!-- ✅ Close button -->
+                <button type="button"
+                    class="position-absolute top-0 end-0 m-3 z-3 d-flex align-items-center justify-content-center"
+                    data-bs-dismiss="modal" aria-label="Close"
+                    style="
+                    width: 40px;
+                    height: 40px;
+                    background-color: #dc3545;
+                    border: none;
+                    border-radius: 50%;
+                    color: #fff;
+                    font-size: 20px;
+                    font-weight: bold;
+                ">
+                    &times;
+                </button>
+
+                <div class="modal-body p-0">
+                    <div class="ratio ratio-16x9">
+                        <iframe id="videoIframe" src="{{ $section->link_image }}" allow="autoplay; fullscreen"
+                            allowfullscreen>
+                        </iframe>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    @endif
+    </div>
+@endif
+@push('scripts')
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const modal = document.getElementById('videoModal');
+            const iframe = document.getElementById('videoIframe');
+
+            modal.addEventListener('hidden.bs.modal', function() {
+                // Stop video by resetting src
+                iframe.src = iframe.src;
+            });
+        });
+    </script>
+@endpush
--- resources/views/frontend/pages/sections/video.blade.php
+++ resources/views/frontend/pages/sections/video.blade.php
@@ -2,34 +2,34 @@
     $layout = $section->layout ?? 'image_left';
 @endphp
 
-<section class="py-5" style="{{ $section->style }}>
+<section class="py-5" style="{{ $section->style }}">
 
     <div class="container">
 
-    @if ($layout === 'full')
-        <div class="row justify-content-center">
-            <div class="col-lg-10">
-                @include('frontend.pages.sections.partials.content')
-                @include('frontend.pages.sections.partials.video')
-            </div>
-        </div>
-    @else
-        <div class="row align-items-center g-5">
-            @if ($layout === 'image_left')
-                <div class="col-lg-5">
+        @if ($layout === 'full')
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    @include('frontend.pages.sections.partials.content')
                     @include('frontend.pages.sections.partials.video')
                 </div>
-            @endif
-            <div class="col-lg-7">
-                @include('frontend.pages.sections.partials.content')
             </div>
-            @if ($layout === 'image_right')
-                <div class="col-lg-5">
-                    @include('frontend.pages.sections.partials.video')
+        @else
+            <div class="row align-items-center g-5">
+                @if ($layout === 'image_left')
+                    <div class="col-lg-5">
+                        @include('frontend.pages.sections.partials.video')
+                    </div>
+                @endif
+                <div class="col-lg-7">
+                    @include('frontend.pages.sections.partials.content')
                 </div>
-            @endif
-        </div>
-    @endif
+                @if ($layout === 'image_right')
+                    <div class="col-lg-5">
+                        @include('frontend.pages.sections.partials.video')
+                    </div>
+                @endif
+            </div>
+        @endif
     </div>
 
 </section>
