f6f66743bbf8c90f2f390ef12db04d721ec89eed
wlsfernandes <wlsfernandes@gmail.com>
2026-04-09 18:44:23 -0400

    new gallery carousel featuers

--- resources/views/frontend/pages/sections/gallery.blade.php
+++ resources/views/frontend/pages/sections/gallery.blade.php
@@ -1,72 +1,68 @@
-<!--Team Section Here-->
-<section class="team__section pt-130 pb-130 overhid" style="{{ $section->style }}>
+<section class="team__section pt-130 pb-130 overhid" style="{{ $section->style }}">
     <div class="container">
 
         <div class="title__content center wow fadeInUp" data-wow-duration="1.3s">
             <div class="cms-html mb-4">
                 {!! $section->getTitle() !!}
             </div>
+
             <div class="witr_bar_main">
                 <div class="witr_bar_inner witr_bar_innerc center"></div>
+
                 <div class="cms-html mb-4">
                     {!! $section->getContent() !!}
                 </div>
             </div>
         </div>
 
-        <div id="sectionGallery{{ $section->id }}" class="carousel slide" data-bs-ride="carousel">
+        {{-- 🔥 GALLERY CAROUSEL --}}
+        <div id="galleryCarousel{{ $section->id }}" class="carousel slide gallery-carousel" data-bs-ride="carousel">
 
+            {{-- MAIN --}}
             <div class="carousel-inner">
+                @foreach ($section->images as $index => $image)
+                    <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                        <div class="gallery-main">
 
-                @foreach ($section->images->chunk(4) as $chunkIndex => $images)
-                    <div class="carousel-item {{ $chunkIndex == 0 ? 'active' : '' }}">
-
-                        <div class="row g-4">
-
-                            @foreach ($images as $image)
-                                <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-4">
-
-                                    <div class="team__items position-relative">
-
-                                        <div class="team__thumb">
-
-                                            <img src="{{ route('admin.images.preview', [
-                                                'model' => 'section_images',
-                                                'id' => $image->id,
-                                            ]) }}"
-                                                alt="Gallery Image" loading="lazy">
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            @endforeach
+                            <img src="{{ route('admin.images.preview', [
+                                'model' => 'section_images',
+                                'id' => $image->id,
+                            ]) }}"
+                                alt="Gallery Image">
 
                         </div>
-
                     </div>
                 @endforeach
-
             </div>
 
-            {{-- Controls --}}
-            <button class="carousel-control-prev" type="button" data-bs-target="#sectionGallery{{ $section->id }}"
+            {{-- CONTROLS --}}
+            <button class="carousel-control-prev" type="button" data-bs-target="#galleryCarousel{{ $section->id }}"
                 data-bs-slide="prev">
-
                 <span class="carousel-control-prev-icon"></span>
-
             </button>
 
-            <button class="carousel-control-next" type="button" data-bs-target="#sectionGallery{{ $section->id }}"
+            <button class="carousel-control-next" type="button" data-bs-target="#galleryCarousel{{ $section->id }}"
                 data-bs-slide="next">
-
                 <span class="carousel-control-next-icon"></span>
-
             </button>
+
+            {{-- 🔥 THUMBNAILS --}}
+            <div class="gallery-thumbs d-flex justify-content-center flex-wrap mt-4">
+                @foreach ($section->images as $index => $image)
+                    <button type="button" data-bs-target="#galleryCarousel{{ $section->id }}"
+                        data-bs-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}">
+
+                        <img src="{{ route('admin.images.preview', [
+                            'model' => 'section_images',
+                            'id' => $image->id,
+                        ]) }}"
+                            alt="Thumb">
+
+                    </button>
+                @endforeach
+            </div>
 
         </div>
 
     </div>
 </section>
-<!--Team Section End-->
